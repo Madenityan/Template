bb8ae85bdcae29f0fe0dc134b2fc2f4267e0c155
madenityan <marinakorsyn@gmail.com>
2017-12-08 00:23:50 +0200

    added fixed header, slider and corrected section portfolio

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-//
-// $(document).ready(function(){
-//     $('.your-class').slick({
-//         setting-name: setting-value
-//     });
-// });
+$(document).ready(function() {
+    $(window).on("scroll",function(){
+        if ($(window).scrollTop() >= 50) {
+            $('.header-wrapper').addClass('fixed');
+
+        } else {
+            $('.header-wrapper').removeClass('fixed');
+        }
+    });
+
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300
+    });
+
+});
